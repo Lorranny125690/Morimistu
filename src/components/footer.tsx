c2a337c2c381879f8cc8f082954098dbff6aa6eb
Lorrapaz <lorrannyyasmin6@gmail.com>
2025-09-23 11:36:55 -0300

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,17 @@
-import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebook, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 export function Footer() {
   return (
-    <footer className="bg-black border-t border-gray-800 py-6">
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center px-6 text-sm text-gray-400">
-        <div className="flex gap-4 text-lg mb-4 md:mb-0 justify-center md:justify-start">
-          <FaFacebookF className="cursor-pointer hover:text-white transition" />
-          <FaInstagram className="cursor-pointer hover:text-white transition" />
-          <FaLinkedinIn className="cursor-pointer hover:text-white transition" />
+    <footer className="py-6">
+      <div className="max-w-7xl h-20 mx-auto flex flex-col md:flex-row justify-between items-center px-6 text-sm text-white">
+        <div className="flex gap-4 items-center text-lg mb-4 md:mb-0 justify-center md:justify-start">
+          <FaFacebook className="h-7 w-7 cursor-pointer hover:text-gray-400 transition" />
+          <FaLinkedinIn className="h-7 w-7 cursor-pointer hover:text-gray-400 transition" />
+          <FaWhatsapp className="h-7 w-7 cursor-pointer hover:text-gray-400 transition" />
+          <MdOutlineEmail className="h-7 w-7 cursor-pointer hover:text-gray-400 transition"/>
+          <div className="bg-white w-[2px] h-16"/>
+          <text className=""> Redes Sociais</text>
         </div>
         <p className="text-center md:text-right">
           Todos os direitos reservados • Morimitsu Jiu-jitsu
