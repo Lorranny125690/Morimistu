c733deaad40b161e2b5488e27137b156bd17626a
Lorrapaz <lorrannyyasmin6@gmail.com>
2025-10-29 16:21:47 -0300

    Imagens mudadas

--- src/screens/home/components/list.tsx
+++ src/screens/home/components/list.tsx
@@ -1,27 +1,27 @@
 
 export const turmasRecentes = [
-  { id: 1, nome: "Turma 1", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 2, nome: "Turma 2", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 3, nome: "Turma 3", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 1, nome: "Turma 1", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 2, nome: "Turma 2", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 3, nome: "Turma 3", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
+  { id: 1, nome: "Turma 1", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 2, nome: "Turma 2", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 3, nome: "Turma 3", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 1, nome: "Turma 1", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 2, nome: "Turma 2", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 3, nome: "Turma 3", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
 ];
 
 export const alunosGraduar = [
-  { id: 1, nome: "Maria Souza", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 2, nome: "Lucas Andrade", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 3, nome: "Fernanda Lima", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 1, nome: "Maria Souza", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 2, nome: "Lucas Andrade", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 3, nome: "Fernanda Lima", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
+  { id: 1, nome: "Maria Souza", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 2, nome: "Lucas Andrade", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 3, nome: "Fernanda Lima", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 1, nome: "Maria Souza", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 2, nome: "Lucas Andrade", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 3, nome: "Fernanda Lima", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
 ];
 
 export const alunosAniversariando = [
-  { id: 1, nome: "Pedro Henrique", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 2, nome: "Ana Beatriz", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 3, nome: "Carla Silva", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 1, nome: "Maria Souza", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 2, nome: "Lucas Andrade", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
-  { id: 3, nome: "Fernanda Lima", imagem: "https://i.pinimg.com/736x/3c/0c/c5/3c0cc53eadfc3a22b19ecb574f0be460.jpg" },
+  { id: 1, nome: "Pedro Henrique", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 2, nome: "Ana Beatriz", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 3, nome: "Carla Silva", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 1, nome: "Maria Souza", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 2, nome: "Lucas Andrade", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
+  { id: 3, nome: "Fernanda Lima", imagem: "https://i.pinimg.com/736x/27/ca/57/27ca572a2787b088c8e148c9b4f73e84.jpg" },
 ];
